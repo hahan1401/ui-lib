5139815fdcc9ead19344926ca4a3c3eaaba2777a
hahan <hanvietha141@gmail.com>
2024-10-02 16:05:14 +0700

    v0.1.18.5: remove valueless function

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # React + TypeScript + Vite
 
 View my Storybook at https://66fb9da7415b49a3cfb7259d-lnmwaejdzx.chromatic.com/<br />
-npm: https://www.npmjs.com/package/hahan-ui-lib
+npm: https://www.npmjs.com/package/hahan-ui-lib<br />
+github: https://github.com/hahan1401/ui-lib
 
 + deepClone: clone value avoid pass by Reference<br />
     import { deepClone } from 'hahan-ui-lib'
+
++ removeValueLess: remove all null, undefined, [] and {} in complex array or object<br />
+    import { removeValueLess } from 'hahan-ui-lib'
--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,11 +1,49 @@
+const isValuefullArray = (array: Array<unknown>) => array.length > 0;
+const isValuefullObject = (obj: Record<string, unknown>) => Object.keys(obj).length > 0;
+
 export const deepClone = <T>(input: T): T => {
-    if (typeof input !== 'object' || input === null) {
-        return input
-    }
+	if (typeof input !== 'object' || input === null) {
+		return input;
+	}
 
-    if (Array.isArray(input)) {
-        return input.map(_i => deepClone(_i)) as T
-    }
+	if (Array.isArray(input)) {
+		return input.map((_i) => deepClone(_i)) as T;
+	}
 
-    return Object.fromEntries(Object.entries(input).map(([key, value]) => [key, deepClone(value)])) as T
-}
+	return Object.fromEntries(Object.entries(input).map(([key, value]) => [key, deepClone(value)])) as T;
+};
+
+export const removeValueLess = <T>(input: T): T => {
+	if (Array.isArray(input)) {
+		return input
+			.map((item) => removeValueLess(item))
+			.filter((item) => {
+				if (typeof item !== 'object' || item === null) return Boolean(item);
+				if (typeof item == 'object' && item !== null && Array.isArray(item) && !isValuefullArray(item)) return false;
+				if (typeof item == 'object' && item !== null && !Array.isArray(item) && !isValuefullObject(item)) return false;
+				return true;
+			}) as T;
+	}
+	if (typeof input === 'object' && input !== null) {
+		const result: Record<string, unknown> = {};
+		for (const key in input) {
+			const value = removeValueLess(input[key]);
+			if (
+				typeof value === 'object' &&
+				value !== null &&
+				!Array.isArray(value) &&
+				isValuefullObject(value as Record<string, unknown>)
+			) {
+				result[key] = value;
+			}
+			if (typeof value === 'object' && value !== null && Array.isArray(value) && isValuefullArray(value)) {
+				result[key] = value;
+			}
+			if (typeof value !== 'object' && Boolean(value)) {
+				result[key] = value;
+			}
+		}
+		return result as T;
+	}
+	return input;
+};
